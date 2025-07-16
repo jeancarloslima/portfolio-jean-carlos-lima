eab5c28af3c45f3352e0d784f63db10569eab4fc
Jean Carlos Oliveira Lima <jcoliveiralima19@gmail.com>
2025-07-16 13:34:49 -0300

    Adicionando botao de trocar tema da página

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const htmlElemento = document.documentElement;
-const tema = htmlElemento.getAttribute("tema");
+const botaoTrocaTema = document.getElementById("btn-troca-tema");
+const iconteBotaoTrocaTema = document.getElementById("icone-botao-troca-tema");
 const botaoAbreMenuHamburguer = document.getElementById("menu-hamburguer");
 const linksNavegacao = document.querySelectorAll(".link-navegacao");
+let tema = htmlElemento.getAttribute("tema");
+
+botaoTrocaTema.addEventListener("click", () => {
+    if (tema === "escuro") {
+        htmlElemento.setAttribute("tema", "claro");
+        tema = htmlElemento.getAttribute("tema");
+        iconteBotaoTrocaTema.classList.remove("fa-sun");
+        iconteBotaoTrocaTema.classList.add("fa-moon");
+    } else {
+        htmlElemento.setAttribute("tema", "escuro");
+        tema = htmlElemento.getAttribute("tema");
+        iconteBotaoTrocaTema.classList.remove("fa-moon");
+        iconteBotaoTrocaTema.classList.add("fa-sun");
+    }
+});
 
 botaoAbreMenuHamburguer.addEventListener("click", () => {
     botaoAbreMenuHamburguer.classList.toggle("ativo");
